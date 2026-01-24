54ad4c45756709588280365ea72f2bde8acbaeca
varun <varun@ozrit.com>
2026-01-24 15:03:19 +0530

    committed by varun at office 24-01-2026 3:03 PM

--- android/app/build.gradle.kts
+++ android/app/build.gradle.kts
@@ -31,7 +31,7 @@ android {
 
     defaultConfig {
         applicationId = "com.ind.classifieds"
-        minSdkVersion(24) // Correct function syntax
+        minSdkVersion(24)
         targetSdk = flutter.targetSdkVersion
         versionCode = flutter.versionCode
         versionName = flutter.versionName
