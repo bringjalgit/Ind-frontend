5b489ebded4f385620293fa0cf0404efa4eeb28b
varun <varun@ozrit.com>
2026-02-26 15:02:59 +0530

    committed by varun at office 26-02-2026 3:02 PM

--- lib/utils/constants.dart
+++ lib/utils/constants.dart
@@ -2,7 +2,8 @@ import 'package:flutter/material.dart';
 
 final GlobalKey<NavigatorState> navigatorKey = GlobalKey<NavigatorState>();
 
-const String google_map_key = "AIzaSyD0-eauuJ1zBrknaL4uNexkR21cYVOkj7k";
+// const String google_map_key = "AIzaSyD0-eauuJ1zBrknaL4uNexkR21cYVOkj7k";
+const String google_map_key = "AIzaSyBe2_PAbLOZXEF3YlTkB4G3tAUJ5MHHCwQ";
 
 String latlng = "";
 String address = "";
